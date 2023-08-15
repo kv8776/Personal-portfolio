4e32da158c11c9dd1995346bc575a46e77dd28bd
ganesh <kv8776@srmist.edu.in>
2023-08-15 10:56:05 +0530

    modified:   index.html 	new file:   javascript/script.js modified:   style.css
    NEW

---
+++ javascript/script.js
@@ -0,0 +1,35 @@
+
+const dynamicText = document.querySelector(".typing-text");
+    const words = ["Web development", "Web design", "Coding", "Games"];
+    let wordIndex = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+    
+    const typeEffect = () => {
+      const currentWord = words[wordIndex];
+      const currentChar = currentWord.substring(0, charIndex);
+      dynamicText.textContent = currentChar;
+    
+      if (!isDeleting) {
+        charIndex++;
+    
+        if (charIndex > currentWord.length) {
+          isDeleting = true;
+          setTimeout(() => {
+            wordIndex++;
+            isDeleting = false;
+          }, 2000); // Delay before switching to the next word
+        }
+      } else {
+        charIndex--;
+    
+        if (charIndex === 0) {
+          wordIndex = (wordIndex + 1) % words.length; // Loop through the words array
+        }
+      }
+    
+      setTimeout(typeEffect, 150); // Delay between typing/deleting characters
+    };
+    
+    // Initial call to start the typing animation
+    typeEffect();
